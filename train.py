22e06d900d951c8d4212623332aedb1128659166
Yuanzhe Dong <yuanzhedong@gmail.com>
2022-02-08 18:29:24 -0800

    fix moe

--- train.py
+++ train.py
@@ -15,8 +15,10 @@ from torch.utils.data import DataLoader
 from torch.utils.data.sampler import RandomSampler, SequentialSampler
 from models import MoE
 from args import get_train_test_args
+import torch.nn as nn
 
 from tqdm import tqdm
+
 
 def prepare_eval_data(dataset_dict, tokenizer):
     tokenized_examples = tokenizer(dataset_dict['question'],
@@ -48,7 +50,6 @@ def prepare_eval_data(dataset_dict, tokenizer):
         ]
 
     return tokenized_examples
-
 
 
 def prepare_train_data(dataset_dict, tokenizer):
@@ -110,22 +111,22 @@ def prepare_train_data(dataset_dict, tokenizer):
             context = dataset_dict['context'][sample_index]
             offset_st = offsets[tokenized_examples['start_positions'][-1]][0]
             offset_en = offsets[tokenized_examples['end_positions'][-1]][1]
-            if context[offset_st : offset_en] != answer['text'][0]:
+            if context[offset_st: offset_en] != answer['text'][0]:
                 inaccurate += 1
 
     total = len(tokenized_examples['id'])
-    print(f"Preprocessing not completely accurate for {inaccurate}/{total} instances")
+    print(
+        f"Preprocessing not completely accurate for {inaccurate}/{total} instances")
     return tokenized_examples
 
 
-
 def read_and_process(args, tokenizer, dataset_dict, dir_name, dataset_name, split):
-    #TODO: cache this if possible
+    # TODO: cache this if possible
     cache_path = f'{dir_name}/{dataset_name}_encodings.pt'
     if os.path.exists(cache_path) and not args.recompute_features:
         tokenized_examples = util.load_pickle(cache_path)
     else:
-        if split=='train':
+        if split == 'train':
             tokenized_examples = prepare_train_data(dataset_dict, tokenizer)
         else:
             tokenized_examples = prepare_eval_data(dataset_dict, tokenizer)
@@ -133,8 +134,7 @@ def read_and_process(args, tokenizer, dataset_dict, dir_name, dataset_name, spli
     return tokenized_examples
 
 
-
-#TODO: use a logger, use tensorboard
+# TODO: use a logger, use tensorboard
 class Trainer():
     def __init__(self, args, log):
         self.lr = args.lr
@@ -150,7 +150,7 @@ class Trainer():
             os.makedirs(self.path)
 
     def save(self, model, f1_score):
-        #model.save_pretrained(self.path)
+        # model.save_pretrained(self.path)
         # TODO: add save model
         # save_file = os.path.join(self.save_dir, "saved_model_{:.3f}.pt".format(f1_score))
         # save_file_config = os.path.join(self.save_dir, "config_{:.3f}.json".format(f1_score))
@@ -173,9 +173,7 @@ class Trainer():
                 input_ids = batch['input_ids'].to(device)
                 attention_mask = batch['attention_mask'].to(device)
                 batch_size = len(input_ids)
-                logits, aux_loss = model(batch)
-                # Forward
-                start_logits, end_logits = logits.split(384, dim=-1)
+                start_logits, end_logits, aux_loss = model(batch)
                 start_logits = start_logits.squeeze(-1)
                 end_logits = end_logits.squeeze(-1)
                 # TODO: compute loss
@@ -188,8 +186,8 @@ class Trainer():
         start_logits = torch.cat(all_start_logits).cpu().numpy()
         end_logits = torch.cat(all_end_logits).cpu().numpy()
         preds = util.postprocess_qa_predictions(data_dict,
-                                                 data_loader.dataset.encodings,
-                                                 (start_logits, end_logits))
+                                                data_loader.dataset.encodings,
+                                                (start_logits, end_logits))
         if split == 'validation':
             results = util.eval_dicts(data_dict, preds)
             results_list = [('F1', results['F1']),
@@ -224,20 +222,22 @@ class Trainer():
                     #                 start_positions=start_positions,
                     #                 end_positions=end_positions)
 
-                    logits, aux_loss = model(batch)
-                    start_logits, end_logits = logits.split(384, dim=-1)
-                    start_logits = start_logits.squeeze(-1)
-                    end_logits = end_logits.squeeze(-1)
+                    start_logits, end_logits, auc_loss = model(batch)
+                    loss = 0
+                    if start_positions is not None and end_positions is not None:
+                        if len(start_positions.size()) > 1:
+                            start_positions = start_positions.squeeze(-1)
+                        if len(end_positions.size()) > 1:
+                            end_positions = end_positions.squeeze(-1)
+                        # sometimes the start/end positions are outside our model inputs, we ignore these terms
+                        ignored_index = start_logits.size(1)
+                        start_positions = start_positions.clamp(0, ignored_index)
+                        end_positions = end_positions.clamp(0, ignored_index)
 
-                    ignored_index = start_logits.size(1)
-                    start_positions.clamp_(0, ignored_index)
-                    end_positions.clamp_(0, ignored_index)
-
-                    loss_fct = torch.nn.CrossEntropyLoss(ignore_index=ignored_index)
-                    start_loss = loss_fct(start_logits, start_positions)
-                    end_loss = loss_fct(end_logits, end_positions)
-                    qa_loss = (start_loss + end_loss) / 2
-                    loss = qa_loss + aux_loss
+                        loss_fct = nn.CrossEntropyLoss(ignore_index=ignored_index)
+                        start_loss = loss_fct(start_logits, start_positions)
+                        end_loss = loss_fct(end_logits, end_positions)
+                        loss = (start_loss + end_loss) / 2 + auc_loss
                     loss.backward()
                     optim.step()
                     progress_bar.update(len(input_ids))
@@ -245,20 +245,22 @@ class Trainer():
                     tbx.add_scalar('train/NLL', loss.item(), global_idx)
                     if (global_idx % self.eval_every) == 0:
                         self.log.info(f'Evaluating at step {global_idx}...')
-                        preds, curr_score = self.evaluate(model, dev_dataloader, dev_dict, return_preds=True)
-                        results_str = ', '.join(f'{k}: {v:05.2f}' for k, v in curr_score.items())
+                        preds, curr_score = self.evaluate(
+                            model, dev_dataloader, dev_dict, return_preds=True)
+                        results_str = ', '.join(
+                            f'{k}: {v:05.2f}' for k, v in curr_score.items())
                         self.log.info('Visualizing in TensorBoard...')
                         for k, v in curr_score.items():
                             tbx.add_scalar(f'val/{k}', v, global_idx)
                         self.log.info(f'In domain {results_str}')
 
-                        preds, curr_score = self.evaluate(model, ood_dev_dataloader, ood_dev_dict, return_preds=True)
-                        results_str = ', '.join(f'{k}: {v:05.2f}' for k, v in curr_score.items())
+                        preds, curr_score = self.evaluate(
+                            model, ood_dev_dataloader, ood_dev_dict, return_preds=True)
+                        results_str = ', '.join(
+                            f'{k}: {v:05.2f}' for k, v in curr_score.items())
                         for k, v in curr_score.items():
                             tbx.add_scalar(f'oodomain_val/{k}', v, global_idx)
                         self.log.info(f'Out of domain {results_str}')
-
-
 
                         if self.visualize_predictions:
                             util.visualize(tbx,
@@ -273,16 +275,19 @@ class Trainer():
                     global_idx += 1
         return best_scores
 
+
 def get_dataset(args, datasets, data_dir, tokenizer, split_name):
     datasets = datasets.split(',')
     dataset_dict = None
-    dataset_name=''
+    dataset_name = ''
     for dataset in datasets:
         dataset_name += f'_{dataset}'
         dataset_dict_curr = util.read_squad(f'{data_dir}/{dataset}')
         dataset_dict = util.merge(dataset_dict, dataset_dict_curr)
-    data_encodings = read_and_process(args, tokenizer, dataset_dict, data_dir, dataset_name, split_name)
-    return util.QADataset(data_encodings, train=(split_name=='train')), dataset_dict
+    data_encodings = read_and_process(
+        args, tokenizer, dataset_dict, data_dir, dataset_name, split_name)
+    return util.QADataset(data_encodings, train=(split_name == 'train')), dataset_dict
+
 
 def main():
     # define parser and arguments
@@ -294,8 +299,28 @@ def main():
     k = 4
     util.set_seed(args.seed)
     #model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased")
-    model = MoE(input_size, output_size, num_experts,hidden_size, k=k, noisy_gating=True)
-    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    #model = MoE(input_size, output_size, num_experts,hidden_size, k=k, noisy_gating=True)
+    model = MoE(
+        dim=768,
+        # increase the experts (# parameters) of your model without increasing computation
+        num_experts=16,
+        # size of hidden dimension in each expert, defaults to 4 * dimension
+        hidden_dim=512 * 4,
+        activation=nn.LeakyReLU,      # use your preferred activation, will default to GELU
+        # in top_2 gating, policy for whether to use a second-place expert
+        second_policy_train='random',
+        # all (always) | none (never) | threshold (if gate value > the given threshold) | random (if gate value > threshold * random_uniform(0, 1))
+        second_policy_eval='random',
+        second_threshold_train=0.2,
+        second_threshold_eval=0.2,
+        # experts have fixed capacity per batch. we need some extra capacity in case gating is not perfectly balanced.
+        capacity_factor_train=1.25,
+        capacity_factor_eval=2.,      # capacity_factor_* should be set to a value >=1
+        # multiplier on the auxiliary expert balancing auxiliary loss
+        loss_coef=1e-2
+    )
+    tokenizer = DistilBertTokenizerFast.from_pretrained(
+        'distilbert-base-uncased')
 
     if args.do_train:
         if not os.path.exists(args.save_dir):
@@ -304,28 +329,34 @@ def main():
         log = util.get_logger(args.save_dir, 'log_train')
         log.info(f'Args: {json.dumps(vars(args), indent=4, sort_keys=True)}')
         log.info("Preparing Training Data...")
-        args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        args.device = torch.device(
+            'cuda') if torch.cuda.is_available() else torch.device('cpu')
         trainer = Trainer(args, log)
-        train_dataset, _ = get_dataset(args, args.train_datasets, args.train_dir, tokenizer, 'train')
+        train_dataset, _ = get_dataset(
+            args, args.train_datasets, args.train_dir, tokenizer, 'train')
         log.info("Preparing Validation Data...")
-        val_dataset, val_dict = get_dataset(args, args.train_datasets, args.val_dir, tokenizer, 'val')
+        val_dataset, val_dict = get_dataset(
+            args, args.train_datasets, args.val_dir, tokenizer, 'val')
         log.info("Preparing Test Data...")
-        ood_val_dataset, ood_val_dict = get_dataset(args, args.eval_datasets, "datasets/oodomain_val", tokenizer, "val")
+        ood_val_dataset, ood_val_dict = get_dataset(
+            args, args.eval_datasets, "datasets/oodomain_val", tokenizer, "val")
 
         train_loader = DataLoader(train_dataset,
-                                batch_size=args.batch_size,
-                                sampler=RandomSampler(train_dataset))
+                                  batch_size=args.batch_size,
+                                  sampler=RandomSampler(train_dataset))
         val_loader = DataLoader(val_dataset,
                                 batch_size=args.batch_size,
                                 sampler=SequentialSampler(val_dataset))
-        
+
         ood_val_loader = DataLoader(ood_val_dataset,
-                                 batch_size=args.batch_size,
-                                 sampler=SequentialSampler(ood_val_dataset))
-        best_scores = trainer.train(model, train_loader, val_loader, val_dict, ood_val_loader, ood_val_dict)
+                                    batch_size=args.batch_size,
+                                    sampler=SequentialSampler(ood_val_dataset))
+        best_scores = trainer.train(
+            model, train_loader, val_loader, val_dict, ood_val_loader, ood_val_dict)
 
     if args.do_eval:
-        args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        args.device = torch.device(
+            'cuda') if torch.cuda.is_available() else torch.device('cpu')
         split_name = 'test' if 'test' in args.eval_dir else 'validation'
         log = util.get_logger(args.save_dir, f'log_{split_name}')
         trainer = Trainer(args, log)
@@ -333,17 +364,20 @@ def main():
         # TODO: add loading model for MoE
         #model = DistilBertForQuestionAnswering.from_pretrained(checkpoint_path)
         model.to(args.device)
-        eval_dataset, eval_dict = get_dataset(args, args.eval_datasets, args.eval_dir, tokenizer, split_name)
+        eval_dataset, eval_dict = get_dataset(
+            args, args.eval_datasets, args.eval_dir, tokenizer, split_name)
         eval_loader = DataLoader(eval_dataset,
                                  batch_size=args.batch_size,
                                  sampler=SequentialSampler(eval_dataset))
         eval_preds, eval_scores = trainer.evaluate(model, eval_loader,
                                                    eval_dict, return_preds=True,
                                                    split=split_name)
-        results_str = ', '.join(f'{k}: {v:05.2f}' for k, v in eval_scores.items())
+        results_str = ', '.join(
+            f'{k}: {v:05.2f}' for k, v in eval_scores.items())
         log.info(f'Eval {results_str}')
         # Write submission file
-        sub_path = os.path.join(args.save_dir, split_name + '_' + args.sub_file)
+        sub_path = os.path.join(
+            args.save_dir, split_name + '_' + args.sub_file)
         log.info(f'Writing submission file to {sub_path}...')
         with open(sub_path, 'w', newline='', encoding='utf-8') as csv_fh:
             csv_writer = csv.writer(csv_fh, delimiter=',')

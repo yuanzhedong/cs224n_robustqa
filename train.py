216f511986e677a569ee183999107ffe06604e51
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2022-03-07 22:51:09 -0800

    back translate ood train data also

--- train.py
+++ train.py
@@ -526,12 +526,12 @@ def get_dataset(args, tokenizer, split_name, num_aug=0):
             dataset_name = os.path.basename(dataset_path)
             dataset_dict_curr = None
             # for finetuning, back translate first because it is slower than eda
-            if args.back_translate and split_name == "finetune": # also apends orignal sentences
-                print("BACK-TRANSLATE", split_name)
-                dataset_dict_curr = perform_back_translate.perform_back_translate(
-                    args, dataset_path, dataset_name
-                )
-                dataset_dict = util.merge(dataset_dict, dataset_dict_curr)
+            if args.back_translate and split_name in ["train", "finetune"]: # also apends orignal sentences
+                if dataset_name in ["duorc", "race", "relation_extraction"]: # back translate ood data only
+                    dataset_dict_curr = perform_back_translate.perform_back_translate(
+                        args, dataset_path, dataset_name
+                    )
+                    dataset_dict = util.merge(dataset_dict, dataset_dict_curr)
 
             if args.eda and split_name in ["train", "finetune"]:  # eda.py does appends original sentences to augmented sentences
                 dataset_dict_curr = perform_eda.perform_eda(
